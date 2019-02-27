7c30326f22d1a61445902289e60274c168e0018f
Manoj Veluru <z1840907@tiger.cs.niu.edu>
2019-02-27 16:38:57 -0600

    completed code

--- prog2.py
+++ prog2.py
@@ -4,68 +4,78 @@ import re
 import os
 def checkArgs():
 	if len(sys.argv)<3 or len(sys.argv)>3:
-		sys.stderr.write("enter correct usage")
+		sys.stderr.write("\tUsage: ./prog2.py inFile outFile\n\n")
 		sys.exit()
 	else:
 		return sys.argv[1],sys.argv[2]
 
 def openFiles(files):
-	if os.path.isfile(files[0]) and os.path.isfile(files[1]):
-		f = open(files[0])
-		g = open(files[1])
-		return f,g
-	else:
-		print("Can't open file: "+os.path.abspath(files[0]),file=sys.stderr)
-		sys.exit()
-		
+	try:
+		inputFile = open(files[0])
+		outputFile = open(files[1],'w')
+		outputFile.write("Output Values for Data: "+os.path.abspath(files[0])+"\n")
+		return inputFile,outputFile
+	except (OSError, IOError) as e:
+		sys.stderr.write("Can't open file: "+os.path.abspath(files[0])+"\n\n")
+		sys.exit()			
 		
 def closeFiles(fobjects):
 	fobjects[0].close()
 	fobjects[1].close()
+	if not (fobjects[0].closed or fobjects[1].closed):
+		sys.stderr.write("File cannot be closed")
+		sys.exit()
 
 def createList(inFileObj):
 	words=[]
-	#mystr.replace('-', ' ').split(' ')
+	wo=""
 	for line in inFileObj[0]:
-		#line = line.replace("'","-")
 		for word in line.replace('-',' ').split(' '):
 			word = word.strip()
-			words.append(word)
-			
+			words.append(word)								
 	return words
 	
 def createDictionary(words):
-	s =[]
-	#print(words)
-	#regex = re.compile('[^a-zA-Z]')
-	for wor in words:
-		sub = "'"
-		subb= "\n"
-		if sub in wor:
-			wor = wor.split("'")
-			#print(ge[0].lower())
-			s.append(wor[0].lower())
-		elif subb in wor:
-			wor = wor.replace(" ","")
-		else:	
-			print(wor)
-			result = re.sub('[^a-zA-Z]', "", wor)
-			s.append(result.lower())
+	newWords =[]
+	for element in words:
+		quote = "'"
+		questionmark= "?"
+		if quote in element:
+			element = element.split("'")
+			if element[0].isalpha():
+				newWords.append(element[0].lower())
+		elif questionmark in element:
+			element = element.split("?")
+			if element[0].isalpha():
+				newWords.append(element[0].lower())
+		else:		
+			result = re.sub('[^a-zA-Z]', "", element)
+			if result.isalpha():
+				newWords.append(result.lower())
+			
 	dic = {}
-	for w in s:
-		dic[w] = s.count(w)
-	
-	for key in sorted(dic.keys()):
-		#print ("%s: %s" % (key, dic[key]))
-		print("",end='')
-	print(len(dic))
-	#print(sorted(dic))
+	for key in newWords:
+		dic[key] = newWords.count(key)
+	return dic
 	
 def printDictionary():
 	fileObj = openFiles(checkArgs())
 	wordsList = createList(fileObj)
-	createDictionary(wordsList)
+	dic = createDictionary(wordsList)
+	i=0
+	fileObj[1].write("---------------------------------------------------------\n")
+	fileObj[1].write("size = "+str(len(dic)))
+	fileObj[1].write("\n\n")
+	for key in sorted(dic.keys()):
+		if i<3:
+			fileObj[1].write ("%-14s: %3s" % (key, dic[key])+"    ")
+			i=i+1
+		else:
+			fileObj[1].write("\n")
+			fileObj[1].write ("%-14s: %3s" % (key, dic[key])+"    ")
+			i=1
+	
 	closeFiles(fileObj)
 	
-#createDictionary(createList(openFiles(checkArgs())))
+
 printDictionary()			
